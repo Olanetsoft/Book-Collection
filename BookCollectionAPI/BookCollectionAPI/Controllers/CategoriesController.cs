c98fb1331cf21bc1514083f620a76f71667cd2c9
Idris Olubisi <heedris2olubisi@gmail.com>
2021-01-31 23:34:49 +0100

    Added categories controller

---
+++ BookCollectionAPI/BookCollectionAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,22 @@
+﻿using BookCollectionAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookCollectionAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : Controller
+    {
+        private ICategoriesRepository _categoriesRepository;
+
+        // Constructor
+        public CategoriesController(ICategoriesRepository categoriesRepository)
+        {
+            _categoriesRepository = categoriesRepository;
+        }
+    }
+}
