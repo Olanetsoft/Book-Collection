a9a4894af8f3994535d8f429ff4846e8373a6623
Idris Olubisi <heedris2olubisi@gmail.com>
2021-01-31 23:22:38 +0100

    Implemented categories interface

---
+++ BookCollectionAPI/BookCollectionAPI/Services/CategoriesRepository.cs
@@ -0,0 +1,36 @@
+﻿using BookCollectionAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookCollectionAPI.Services
+{
+    public class CategoriesRepository : ICategoriesRepository
+    {
+        public bool CategoryExists(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<BookCategory> GetBooksForCategory(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Category> GetCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<BookCategory> GetCategoriesOfABook(int bookId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category GetCatgory(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BookCollectionAPI/BookCollectionAPI/Services/ICategoriesRepository.cs
@@ -0,0 +1,17 @@
+﻿using BookCollectionAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookCollectionAPI.Services
+{
+    public interface ICategoriesRepository
+    {
+        ICollection<Category> GetCategories();
+        Category GetCatgory(int categoryId);
+        ICollection<BookCategory> GetCategoriesOfABook(int bookId);
+        ICollection<BookCategory> GetBooksForCategory(int categoryId);
+        bool CategoryExists(int categoryId);
+    }
+}
