93de4e08640b714871b553a4cb2b60f8e5eae3d1
Idris Olubisi <heedris2olubisi@gmail.com>
2021-02-02 13:37:30 +0100

    Implemented all get request in book repository

---
+++ BookCollectionAPI/BookCollectionAPI/Controllers/BooksController.cs
@@ -0,0 +1,132 @@
+﻿using BookCollectionAPI.Dtos;
+using BookCollectionAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookCollectionAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : Controller
+    {
+        private IBookRepository _bookRepository;
+        private IAuthorRepository _authorRepository;
+        private ICategoriesRepository _categoryRepository;
+        private IReviewRepository _reviewRepository;
+
+        public BooksController(IBookRepository bookRepository, IAuthorRepository authorRepository,
+          ICategoriesRepository categoryRepository, IReviewRepository reviewRepository)
+        {
+            _bookRepository = bookRepository;
+            _authorRepository = authorRepository;
+            _categoryRepository = categoryRepository;
+            _reviewRepository = reviewRepository;
+        }
+
+
+
+        //api/books
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<BookDto>))]
+        [ProducesResponseType(400)]
+        public IActionResult GetBooks()
+        {
+            var books = _bookRepository.GetBooks();
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var booksDto = new List<BookDto>();
+
+            foreach (var book in books)
+            {
+                booksDto.Add(new BookDto
+                {
+                    Id = book.Id,
+                    Title = book.Title,
+                    Isbn = book.Isbn,
+                    DatePublished = book.DatePublished
+                });
+            }
+
+            return Ok(booksDto);
+        }
+
+        //api/books/bookId
+        [HttpGet("{bookId}", Name = "GetBook")]
+        [ProducesResponseType(200, Type = typeof(BookDto))]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public IActionResult GetBook(int bookId)
+        {
+            if (!_bookRepository.BookExists(bookId))
+                return NotFound();
+
+            var book = _bookRepository.GetBook(bookId);
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var bookDto = new BookDto()
+            {
+                Id = book.Id,
+                Title = book.Title,
+                Isbn = book.Isbn,
+                DatePublished = book.DatePublished
+            };
+
+            return Ok(bookDto);
+        }
+
+
+
+        //api/books/isbn/bookIsbn
+        [HttpGet("ISBN/{bookIsbn}")]
+        [ProducesResponseType(200, Type = typeof(BookDto))]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public IActionResult GetBook(string bookIsbn)
+        {
+            if (!_bookRepository.BookExists(bookIsbn))
+                return NotFound();
+
+            var book = _bookRepository.GetBook(bookIsbn);
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var bookDto = new BookDto()
+            {
+                Id = book.Id,
+                Title = book.Title,
+                Isbn = book.Isbn,
+                DatePublished = book.DatePublished
+            };
+
+            return Ok(bookDto);
+        }
+
+
+        //api/books/bookId/rating
+        [HttpGet("{bookId}/rating")]
+        [ProducesResponseType(200, Type = typeof(decimal))]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public IActionResult GetBookRating(int bookId)
+        {
+            if (!_bookRepository.BookExists(bookId))
+                return NotFound();
+
+            var rating = _bookRepository.GetBookRating(bookId);
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(rating);
+        }
+
+    }
+}
